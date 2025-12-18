a22656f91cec170714a9fbaa8d19febcf652cd0c
Andy00006 <andy.laloum8@gmail.com>
2025-12-18 11:47:01 +0100

    Refactor main function for improved argument handling

--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,88 +5,30 @@
 #include "data.h"
 
 int main(int argc, char **argv) {
-    if (argc < 2) {
-        fprintf(stderr,
-            "Usage:\n"
-            "  %s histo <csv> <max|src|real>\n"
-            "  %s leaks <csv> \"FactoryID\"\n",
-            argv[0], argv[0]);
-        return 1;
-    }
+    if (argc < 4) return 1;
 
-    if (strcmp(argv[1], "histo") == 0) {
-        if (argc != 4) {
-            fprintf(stderr, "histo requiert: <csv> <max|src|real>\n");
-            return 2;
-        }
+    const char *action = argv[1];
+    const char *csv = argv[2];
+    const char *param = argv[3];
 
-        const char *csv = argv[2];
-        const char *mode = argv[3];
+    AVL *avl_usines = avl_creer();
+    AVL *avl_noeuds = avl_creer();
 
-        if (!(strcmp(mode, "max") == 0 ||
-              strcmp(mode, "src") == 0 ||
-              strcmp(mode, "real") == 0)) {
-            fprintf(stderr, "mode inconnu %s\n", mode);
-            return 3;
-        }
-
-        AVL *avl_usines = avl_creer();
-        AVL *avl_noeuds = avl_creer();
-
-        if (construire_depuis_csv(csv, avl_usines, avl_noeuds) != 0) {
-            /* Libération avec les fonctions appropriées */
-            avl_liberer(avl_usines, liberer_usine_val); 
-            avl_liberer(avl_noeuds, liberer_noeud_val);
-            return 4;
-        }
-
-        if (generer_histo(avl_usines, mode) != 0) {
-            fprintf(stderr, "Erreur lors de la génération de l'histogramme.\n");
-        }
-
-        /* libération COMPLÈTE (CORRECTION) */
+    if (construire_depuis_csv(csv, avl_usines, avl_noeuds) != 0) {
         avl_liberer(avl_usines, liberer_usine_val);
         avl_liberer(avl_noeuds, liberer_noeud_val);
-
-        printf("Fichier vol_%s.dat créé\n", mode);
-        return 0;
+        return 2;
     }
 
-    else if (strcmp(argv[1], "leaks") == 0) {
-        if (argc != 4) {
-            fprintf(stderr, "leaks requiert: <csv> \"FactoryID\"\n");
-            return 5;
-        }
-
-        const char *csv = argv[2];
-        const char *id = argv[3];
-
-        AVL *avl_usines = avl_creer();
-        AVL *avl_noeuds = avl_creer();
-
-        if (construire_depuis_csv(csv, avl_usines, avl_noeuds) != 0) {
-            /* Libération avec les fonctions appropriées */
-            avl_liberer(avl_usines, liberer_usine_val);
-            avl_liberer(avl_noeuds, liberer_noeud_val);
-            return 6;
-        }
-
-        double res = calculer_fuites(avl_usines, avl_noeuds, id);
-
-        if (res < 0.0)
-            printf("-1\n");
-        else
-            printf("%.6f\n", res);
-
-        /* libération COMPLÈTE (CORRECTION) */
-        avl_liberer(avl_usines, liberer_usine_val);
-        avl_liberer(avl_noeuds, liberer_noeud_val);
-
-        return 0;
+    if (strcmp(action, "histo") == 0) {
+        generer_histo(avl_usines, param);
+    } else if (strcmp(action, "leaks") == 0) {
+        double res = calculer_fuites(avl_usines, avl_noeuds, param);
+        if (res < 0) printf("-1\n");
+        else printf("%.6f\n", res);
     }
 
-    else {
-        fprintf(stderr, "Commande inconnue : %s\n", argv[1]);
-        return 7;
-    }
+    avl_liberer(avl_usines, liberer_usine_val);
+    avl_liberer(avl_noeuds, liberer_noeud_val);
+    return 0;
 }

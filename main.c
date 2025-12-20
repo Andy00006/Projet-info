8d1599ba7cd43c9bd43bf2d61ea21d5354b5a547
Andy00006 <andy.laloum8@gmail.com>
2025-12-20 15:08:34 +0100

    Update main.c

--- main.c
+++ main.c
@@ -3,70 +3,82 @@
 #include <string.h>
 #include "avl.h"
 
+// Déclaration de la fonction externe définie dans leaks.c
+extern void executer_mode_leaks(Noeud* racine, char* target_id);
+
 int main(int argc, char* argv[]) {
-    if (argc < 4) return 1;
+    // 1. Vérification des arguments
+    if (argc < 4) {
+        fprintf(stderr, "Erreur : Arguments manquants.\n");
+        return 1;
+    }
 
     FILE* fichier = fopen(argv[1], "r");
-    if (!fichier) return 2;
+    if (!fichier) {
+        fprintf(stderr, "Erreur : Impossible d'ouvrir le fichier %s\n", argv[1]);
+        return 2;
+    }
 
     char* mode = argv[2];
     char* option_id = argv[3];
     Noeud* racine = NULL;
     char ligne[1024];
-    
-    int usine_trouvee = 0;
-    double total_fuites = 0;
 
+    // 2. Lecture du fichier CSV
     while (fgets(ligne, sizeof(ligne), fichier)) {
-        if (ligne[0] == '\n') continue;
-        // NOTE: On ne saute plus les lignes commençant par '-' car tes données en ont !
+        if (ligne[0] == '\n' || ligne[0] == '\r') continue;
 
-        char *c1 = strtok(ligne, ";"); 
-        char *c2 = strtok(NULL, ";");  
-        char *c3 = strtok(NULL, ";");  
-        char *c4 = strtok(NULL, ";");  
-        char *c5 = strtok(NULL, ";\n"); 
+        char *c1 = strtok(ligne, ";"); // Usine ID (Col 1)
+        char *c2 = strtok(NULL, ";");  // Amont ID (Col 2)
+        char *c3 = strtok(NULL, ";");  // Aval ID (Col 3)
+        char *c4 = strtok(NULL, ";");  // Volume/Capacité (Col 4)
+        char *c5 = strtok(NULL, ";\n"); // % Fuite (Col 5)
 
-        double v4 = (c4) ? atof(c4) : 0.0;
-        double v5 = (c5) ? atof(c5) : 0.0;
+        double val4 = (c4 && c4[0] != '-') ? atof(c4) : 0.0;
+        double val5 = (c5 && c5[0] != '-') ? atof(c5) : 0.0;
 
         if (strcmp(mode, "histo") == 0) {
-            // Dans tes données : c2 est la source/amont, c3 est l'usine/aval
-            if (c2 && c3 && strcmp(c3, "-") != 0) {
-                // On calcule le volume traité qui arrive à l'usine (c3)
-                double v_traite = v4 * (1.0 - (v5 / 100.0));
-                // On insère l'usine (c3) avec le volume capté (v4) et traité
-                racine = inserer(racine, c3, v4, v4, v_traite);
+            // --- MODE HISTO ---
+            if (c2 && strstr(c2, "Facility") && (!c3 || c3[0] == '-')) {
+                racine = inserer(racine, c2, val4, 0, 0);
+            }
+            else if (c2 && strstr(c2, "Spring")) {
+                double volume_traite = val4 * (1.0 - (val5 / 100.0));
+                // Astuce : on met val4 dans la colonne max pour que l'option 'max' fonctionne
+                racine = inserer(racine, c3, val4, val4, volume_traite);
             }
         } 
         else if (strcmp(mode, "leaks") == 0) {
-            // Pour le mode leaks, on vérifie si l'ID d'usine (option_id) est en c3
-            if (c3 && strcmp(c3, option_id) == 0) {
-                usine_trouvee = 1;
-                total_fuites += (v4 * (v5 / 100.0));
+            // --- MODE LEAKS ---
+            // On stocke tous les tronçons dans l'AVL pour le calcul récursif
+            if (c3 && strcmp(c3, "-") != 0) {
+                double volume_traite = val4 * (1.0 - (val5 / 100.0));
+                racine = inserer(racine, c3, 0, val4, volume_traite);
             }
         }
     }
     fclose(fichier);
 
-    // --- SORTIE DES DONNÉES ---
-    char nom_sortie[256];
+    // 3. Exportation et Traitement final
     if (strcmp(mode, "histo") == 0) {
-        snprintf(nom_sortie, sizeof(nom_sortie), "resultat_%s.dat", option_id);
+        // Sortie standard pour les graphiques
+        char nom_sortie[128];
+        sprintf(nom_sortie, "resultat_%s.dat", option_id);
         FILE* flux_out = fopen(nom_sortie, "w");
         if (flux_out) {
             fprintf(flux_out, "identifiant;volume_max;volume_capte;volume_traite\n");
             exporter_infixe_inverse(racine, flux_out);
             fclose(flux_out);
         }
-    } else if (strcmp(mode, "leaks") == 0) {
-        FILE* f_hist = fopen("rendement_historique.dat", "a");
-        if (f_hist) {
-            fprintf(f_hist, "%s;%.3f\n", option_id, usine_trouvee ? (total_fuites / 1000.0) : -1.0);
-            fclose(f_hist);
-        }
+    } 
+    else if (strcmp(mode, "leaks") == 0) {
+        // Appel de la logique métier dans leaks.c
+        // Cette fonction s'occupe de calculer et d'écrire dans rendement_historique.dat
+        executer_mode_leaks(racine, option_id);
     }
 
-    if (racine) liberer_arbre(racine);
+    // 4. Nettoyage
+    liberer_arbre(racine);
+
     return 0;
 }

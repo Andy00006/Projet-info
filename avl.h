450a8d3aa251418f6d9d1ccd2764339b22a19a32
Andy00006 <andy.laloum8@gmail.com>
2025-12-21 13:10:50 +0100

    Update avl.h

--- avl.h
+++ avl.h
@@ -2,6 +2,7 @@
 #define AVL_H
 
 #include <stdio.h>
+
 typedef struct Noeud {
     char identifiant[100];
     double capacite_max;
@@ -11,8 +12,11 @@ typedef struct Noeud {
     struct Noeud *gauche;
     struct Noeud *droit;
 } Noeud;
+
 Noeud* inserer(Noeud* noeud, char* id, double capa, double capt, double trait);
 void liberer_arbre(Noeud* racine);
 void exporter_infixe_inverse(Noeud* racine, FILE* flux_sortie);
+Noeud* rechercher_usine(Noeud* r, char* id);
+void executer_mode_leaks(Noeud* racine, char* target_id);
 
 #endif
